232aff8760983f9c62b7947f7950c4d7073cc4a5
Ray Dunning <dunningray@gmail.com>
2019-12-17 09:54:35 -0600

    balanced bracket attempt

--- balancedBrackets/prompt.js
+++ balancedBrackets/prompt.js
@@ -9,4 +9,29 @@
 // str: "[{()}]"                  |   true
 // str: "(50)("                   |   false
 // str: "[{]}"                    |   false
-const isBalanced = str => {};
+const isBalanced = str => {
+    //hold the result boolean and default to true
+    const result = true;
+    // use a stack to store all opening brackets
+    const stack = [];
+    // Create key/value pairs to know which opening bracket goes with
+    const currectBracket = {
+        '}': '{',
+        ']': '[',
+        ')': '(',
+    }
+    //iterate through str
+    //if there push in the bracket and then check if it has a match 
+    for(let i = 0; i < str.length; i++){
+      const current = str.charAt(i);
+      if(current === '{', current === '||', current ===  '[' || '(')
+    }
+
+
+    
+
+};
+
+
+
+  
