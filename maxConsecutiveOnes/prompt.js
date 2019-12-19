c949c0e81fbcc87b8419b7a8502120e5cb150f73
Ray Dunning <dunningray@gmail.com>
2019-12-19 09:58:26 -0600

    maxConsectutive sudo code

--- maxConsecutiveOnes/prompt.js
+++ maxConsecutiveOnes/prompt.js
@@ -2,6 +2,21 @@
 
 //maxConsecutiveOnes([1,1,2,0,3,1,1,1,1,5,6,3]) returns 4
 
+//create a count variable
+//make 2nd variable for max count
+//iterate through the array
+//check if element is 1
+//yes -> increment count variable
+//if count is greater than max, update max
+//no -> reset count variable
+//return max
+
+
+
 function maxConsecutiveOnes(array) {
-  //write solution code
-}
+  // const count = 0;
+  // const countMax = 0;
+  // for(let i = 0; i < array.length; i++){
+// }
+  }
+
