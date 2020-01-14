3f1c8560de7a6261b0a2363334199d37ac8a1939
Ray Dunning <dunningray@gmail.com>
2020-01-14 09:54:01 -0600

    boggle solve Attempted

--- boggleSolve/prompt.js
+++ boggleSolve/prompt.js
@@ -3,4 +3,19 @@
 // * i.e. a=1, z=26 etc.
 // return the sum of the values of each letter in string
 
-const boggleSolve = str => {};
+const boggleSolve = str => {
+    //create an array with all 26 letters in the alphabet
+    const newArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    //create a total count variable
+    const total = 0;
+    if(str){
+        return total;
+    }
+    //iterate through the array
+    for(let i = 0; i < newArr.length; i++){
+        if(newArr[i] === str){
+            total += i - 1
+        }
+
+    }
+};
